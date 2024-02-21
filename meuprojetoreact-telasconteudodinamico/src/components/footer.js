90eb6856c32db3e85cc21443a1767c8480d47c86
vtsilvaa <160682253+vtsilvaa@users.noreply.github.com>
2024-02-21 02:39:03 -0300

    Delete meuprojetoreact-telasconteudodinamico/src directory

--- meuprojetoreact-telasconteudodinamico/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <footer>
-        <p>&copy; 2019</p>
-    </footer>
-);
-
-export default Footer;
--- meuprojetoreact-telasconteudodinamico/src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-    <header>
-        <h1>Meu Projeto React</h1>
-    </header>
-);
-
-export default Header;
